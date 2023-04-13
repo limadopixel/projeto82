bc6475f8455fd97a80ee32b6a9be702e491b1d96
limadopixel <130712451+limadopixel@users.noreply.github.com>
2023-04-13 20:09:25 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var mouseEvent = "empty";
+    canvas = document.getElementById("myCanvas");
+    ctx = canvas.getContext("2d");
+    color = black
+    widthline = 2;
+    canvas.addEventListener("mousedown",mymousedown)
+    function mymousedown(e)
+    {
+    color=document.getElementById("color").value;
+    widthline = Document.getElementById("widthLine"),value;
+
+
+    }
